2b3eae63fcd14b7436195305786089deb954da67
morty0824 <106510733+morty0824@users.noreply.github.com>
2022-07-19 12:53:06 +0800

    feat: 初始化项目

---
+++ src/components/ElementDemo.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup name="element-demo">
+</script>
+
+<template>
+  <!-- <el-table :border="true" stripe :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table> -->
+  <div mt-5>
+    <el-tag>Tag 1</el-tag>
+    <el-tag class="ml-2" type="success">
+      Tag 2
+    </el-tag>
+    <el-tag class="ml-2" type="info">
+      Tag 3
+    </el-tag>
+    <el-tag class="ml-2" type="warning">
+      Tag 4
+    </el-tag>
+    <el-tag class="ml-2" type="danger">
+      Tag 5
+    </el-tag>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+const user = useUserStore()
+const name = $ref(user.savedName)
+const router = useRouter()
+const a = {
+  a: 1,
+  b: 2,
+}
+console.log(get({ a: 'morty' }, 'a'))
+const go = () => {
+  if (name)
+    router.push(`/hi/${encodeURIComponent(name)}`)
+}
+const val = ref('')
+const { t } = useI18n()
+</script>
+
+<template>
+  <div>
+    <div text-4xl>
+      <div i-carbon-campsite inline-block />
+      <el-button>Default</el-button>
+      <el-button type="primary">
+        Primary
+      </el-button>
+      <el-button type="success">
+        Success
+      </el-button>
+      <el-button type="info">
+        Info
+      </el-button>
+      <el-button type="warning">
+        Warning
+      </el-button>
+      <el-button type="danger">
+        Danger
+      </el-button>
+      <el-button>中文</el-button>
+      <el-button type="primary">
+        搜索
+      </el-button>
+      <el-input v-model="val" />
+    </div>
+    <element-demo />
+    <p>
+      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
+        Vitesse
+      </a>
+    </p>
+    <p>
+      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
+    </p>
+
+    <div py-4 />
+
+    <input
+      id="input"
+      v-model="name"
+      :placeholder="t('intro.whats-your-name')"
+      :aria-label="t('intro.whats-your-name')"
+      type="text"
+      autocomplete="false"
+      p="x4 y2"
+      w="250px"
+      text="center"
+      bg="transparent"
+      border="~ rounded gray-200 dark:gray-700"
+      outline="none active:none"
+      @keydown.enter="go"
+    >
+    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
+
+    <div>
+      <button
+        btn m-3 text-sm
+        :disabled="!name"
+        @click="go"
+      >
+        {{ t('button.go') }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
